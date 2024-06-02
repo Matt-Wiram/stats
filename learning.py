749e84a52f5ba915c78ed12d028028eabdbf54e5
unknown <matthew.wiram551@gmail.com>
2024-06-02 15:58:23 -0400

    done

--- learning.py
+++ learning.py
@@ -10,16 +10,34 @@
 # for x in pstring:
 #     print (x)
 # print(len(pstring))
-x = [0]
-def fibonacci():
+import scipy
 
-    while len(x) < 30:
-         if len(x) == 1:
-             x.append(1)
-             print (x)
-         else:
-            x.append(x[-1] + x[-2])
-            print(x)
+# x = [0]
+# def fibonacci():
+#
+#     while len(x) < 30:
+#          if len(x) == 1:
+#              x.append(1)
+#              print (x)
+#          else:
+#             x.append(x[-1] + x[-2])
+#             print(x)
+#
+# def kiloToMiles(x):
+#     return x * 0.62137119
+#
+# def milesToKilo(x):
+#     return x /  0.62137119
+#
+# print (kiloToMiles(3))
+#
+# print (milesToKilo(1.8641135699999998))
+#
+# def triarea(l,w):
+#     return (l*w)/2
+# print (triarea(3,5))
+#
+# name = input("What is your name? ")
+# print ("Hello " + name)
 
-
-fibonacci()
+print(scipy.stats.ttest_ind_from_stats(64.98,23.95,30,76.59,26.21,35,True,"two-sided"))
--- statistical_methods.py
+++ statistical_methods.py
@@ -1,56 +1,74 @@
 import math
 import numpy
 import scipy
+
+
+
+
+def dfgetter(n1, n2):
+   return n1 + n2 -2
+
+
 # pooled sigma
-def pooledsigma(n1,n2,s1,s2):
-    numerator = ((n1-1)*s1**2)+((n2-1)*s2**2)
+def pooledsigma(n1, n2, s1, s2):
+    numerator = ((n1 - 1) * s1 ** 2) + ((n2 - 1) * s2 ** 2)
     denominator = n1 + n2 - 2
-    return math.sqrt(numerator/denominator)
+    print(math.sqrt(numerator/denominator))
+    return math.sqrt(numerator / denominator)
+
+
 # pooled t value
-def tob(x1,x2,n1,n2,sigma):
-    numerator = x1-x2
-    denominator = sigma * (math.sqrt((1/n1)+(1/n2)))
-    return numerator/denominator
+def tob(x1, x2, n1, n2, sigma):
+    numerator = x1 - x2
+    denominator = sigma * (math.sqrt((1 / n1) + (1 / n2)))
+    return numerator / denominator
+
+
 # pooled t crit value for invT(a,df)
 
-def tcritval(a,df):
-    alpha = scipy.stats.t.ppf(1-a,df)
-    print (alpha)
+def tcritval(a, df):
+    if int(testtype) < 2:
+         alpha = scipy.stats.t.ppf(1 - a, df)
+         print(alpha)
+         return alpha
+    else:
+        alpha = scipy.stats.t.ppf(1-(a/2),df)
+        print(alpha)
+        return alpha
+
 
 # p value from plugging in tob to t.cdf(-100000000,-|tcrit|,0,1) equation for not equal to hypothesis
-def pvaluenotequal(tcrit,df):
-    print (2*(scipy.stats.t.cdf(-abs(tcrit),df)))
+def pvalue(tcrit, df):
+    if int(testtype) < 2:
+        print(scipy.stats.t.cdf(tcrit,df))
 
-sigmap = pooledsigma(30,35,23.95,26.21)
-print (sigmap)
+    else:
+        print(2 * (scipy.stats.t.cdf(-abs(tcrit), df)))
 
-tob1 = tob(64.98,76.59,30,35,sigmap)
-print (tob1)
 
-tcritval(0.025,63)
-
-pvaluenotequal(tob1,63)
-
-# name = input("What is your name? ")
-# print ("Hello " + name)
+# sigmap = pooledsigma(30, 35, 23.95, 26.21)
+# print(sigmap)
 #
-# x1=input("What is xbar1? ")
-# x2=input("What is xbar2? ")
-# s1=input("What is sigma1? ")
-# s2=input("What is sigma2? ")
-# n1=input("What is n1? ")
-# n2=input("What is n2? ")
+# tob1 = tob(64.98, 76.59, 30, 35, sigmap)
+# print(tob1)
+#
+# tcritval(0.025, 63)
+#
+# pvalue(tob1, 63)
 
-def kiloToMiles(x):
-    return x * 0.62137119
+testtype = input("What kind of test is this? 0 = M0<MA, 1 = M0>MA, or 2 = M0 not equal MA ")
+x1 = float(input("What is xbar1? "))
+x2 = float(input("What is xbar2? "))
+s1 = float(input("What is sigma1? "))
+s2 = float(input("What is sigma2? "))
+n1 = float(input("What is n1? "))
+n2 = float(input("What is n2? "))
+a = float(input("What is the confidence level? "))
 
-def milesToKilo(x):
-    return x /  0.62137119
 
-print (kiloToMiles(3))
 
-print (milesToKilo(1.8641135699999998))
-
-def triarea(l,w):
-    return (l*w)/2
-print (triarea(3,5))
+df = dfgetter(n1,n2)
+pooledS = pooledsigma(n1,n2,s1,s2)
+tob = tob(x1,x2,n1,n2,pooledS)
+pvalue(tob,df)
+tcritval(a,df)
